c046f8d43ffef52f6acfeda172ede0c01061887d
jnthnch <jchu127@gmail.com>
2020-04-17 23:40:28 -0700

    Commit previous changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const usersRoutes = require("./routes/users");
@@ -8,12 +9,15 @@ app.use(morgan("tiny"));
 app.use(bodyParser.json());
 app.use("/users", usersRoutes);
 
+app.use(express.static(__dirname + '/public'));
+
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  var err = new Error("Not Found");
-  err.status = 404;
-  return next(err);
-});
+// app.use((req, res, next) => {
+//   console.log('got in')
+//   var err = new Error("Not Found");
+//   err.status = 404;
+//   return next(err);
+// });
 
 // development error handler
 // will print stacktrace
