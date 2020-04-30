0d2894e50809a95ddc3db64e3f7e38cc3054fe83
jnthnch <jchu127@gmail.com>
2020-04-29 23:24:51 -0700

    Add backend logic to create selection object

--- src/components/OneMatchup.jsx
+++ src/components/OneMatchup.jsx
@@ -4,20 +4,43 @@ class OneMatchup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: '',
-      disabled: false
+      selection: '',
+      disabled: false,
+      gameId: this.props.game.id
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.createSelectionObject = this.createSelectionObject.bind(this);
     this.addUserSelection = this.props.addUserSelection
   }
 
+  // selection object
+  // id	PRIMARY KEY
+  // game_id	FOREIGN KEY
+  // time	TIME
+  // date	DATE
+  // selection	TEXT
+  // is_won	BOOLEAN
+  // is_push	BOOLEAN
   handleChange(event) {
-    this.setState({ value: event.target.value });
+    this.setState({ selection: event.target.value });
+  }
+
+  createSelectionObject() {
+    let object = {
+      game_id: this.state.gameId,
+      time: null,
+      date: null,
+      selection: this.state.selection,
+      is_won: null,
+      is_push: null
+    }
+    console.log('SELECTION OBJECT IS', object)
   }
 
   handleSubmit(event) {
-    this.addUserSelection(this.state.value)
+    this.createSelectionObject();
+    this.addUserSelection(this.state.selection)
     if (this.state.disabled) {
       return
     } else {
@@ -27,7 +50,7 @@ class OneMatchup extends React.Component {
   }
 
   render() {
-    const { value, disabled } = this.state
+    const { selection, disabled } = this.state
     const { home_team, away_team, home_spread, away_spread } = this.props.game
     const style = {
       backgroundColor: 'gray',
@@ -64,7 +87,7 @@ class OneMatchup extends React.Component {
         <section>
           <form onSubmit={this.handleSubmit}>
             <label>
-              <select value={value} onChange={this.handleChange} required>
+              <select value={selection} onChange={this.handleChange} required>
                 <option value="" hidden>select</option>
                 <option value={`${away_team} ${away_spread}`}>{away_team} {away_spread}</option>
                 <option value={`${home_team} ${home_spread}`}>{home_team} {home_spread}</option>
