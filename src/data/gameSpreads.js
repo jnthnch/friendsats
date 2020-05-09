76024b2217a260fb4fffa1ade6b04a3b07617719
jnthnch <jchu127@gmail.com>
2020-05-09 11:29:07 -0700

    Update game spreads

--- src/data/gameSpreads.js
+++ src/data/gameSpreads.js
@@ -1,37 +1,145 @@
 const gameSpreads = [
   {
     id: 1,
-    homeTeam: '49ers',
-    awayTeam: 'Seahawks',
-    homeSpread: '-3',
-    awaySpread: '+3',
+    homeTeam: 'Chiefs',
+    awayTeam: 'Texans',
+    homeSpread: '-10',
+    awaySpread: '+10',
     homeScore: null,
     awayScore: null
   },
   {
     id: 2,
-    homeTeam: 'Bucs',
-    awayTeam: 'Saints',
-    homeSpread: '-1.5',
-    awaySpread: '+1.5',
+    homeTeam: 'Falcons',
+    awayTeam: 'Seahawks',
+    homeSpread: 'pk',
+    awaySpread: 'pk',
     homeScore: null,
     awayScore: null
   },
   {
     id: 3,
-    homeTeam: 'Raiders',
-    awayTeam: 'Broncos',
-    homeSpread: '-2.5',
-    awaySpread: '+2.5',
+    homeTeam: 'Redskins',
+    awayTeam: 'Eagles',
+    homeSpread: '+6.5',
+    awaySpread: '-6.5',
     homeScore: null,
     awayScore: null
   },
   {
     id: 4,
-    homeTeam: 'Chiefs',
+    homeTeam: 'Patriots',
+    awayTeam: 'Dolphins',
+    homeSpread: '-6.5',
+    awaySpread: '+6.5',
+    homeScore: null,
+    awayScore: null
+  },
+  {
+    id: 5,
+    homeTeam: 'Vikings',
+    awayTeam: 'Packers',
+    homeSpread: '-3.5',
+    awaySpread: '+3.5',
+    homeScore: null,
+    awayScore: null
+  },
+  {
+    id: 6,
+    homeTeam: 'Jaguars',
+    awayTeam: 'Colts',
+    homeSpread: '+7',
+    awaySpread: '-7',
+    homeScore: null,
+    awayScore: null
+  },
+  {
+    id: 7,
+    homeTeam: 'Lions',
+    awayTeam: 'Bears',
+    homeSpread: '+1.5',
+    awaySpread: '-1.5',
+    homeScore: null,
+    awayScore: null
+  },
+  {
+    id: 8,
+    homeTeam: 'Ravens',
+    awayTeam: 'Browns',
+    homeSpread: '-9',
+    awaySpread: '+9',
+    homeScore: null,
+    awayScore: null
+  },
+  {
+    id: 9,
+    homeTeam: 'Bills',
+    awayTeam: 'Jets',
+    homeSpread: '-6',
+    awaySpread: '+6',
+    homeScore: null,
+    awayScore: null
+  },
+  {
+    id: 10,
+    homeTeam: 'Panthers',
+    awayTeam: 'Raiders',
+    homeSpread: 'pk',
+    awaySpread: 'pk',
+    homeScore: null,
+    awayScore: null
+  },
+  {
+    id: 11,
+    homeTeam: 'Bengals',
+    awayTeam: 'Chargers',
+    homeSpread: '+3',
+    awaySpread: '-3',
+    homeScore: null,
+    awayScore: null
+  },
+  {
+    id: 12,
+    homeTeam: 'Saints',
+    awayTeam: 'Buccaneers',
+    homeSpread: '-4.5',
+    awaySpread: '+4.5',
+    homeScore: null,
+    awayScore: null
+  },
+  {
+    id: 13,
+    homeTeam: '49ers',
+    awayTeam: 'Cardinals',
+    homeSpread: '-8',
+    awaySpread: '+8',
+    homeScore: null,
+    awayScore: null
+  },
+  {
+    id: 14,
+    homeTeam: 'Rams',
+    awayTeam: 'Cowboys',
+    homeSpread: '+2.5',
+    awaySpread: '-2.5',
+    homeScore: null,
+    awayScore: null
+  },
+  {
+    id: 15,
+    homeTeam: 'Giants',
+    awayTeam: 'Steelers',
+    homeSpread: '+3',
+    awaySpread: '-3',
+    homeScore: null,
+    awayScore: null
+  },
+  {
+    id: 16,
+    homeTeam: 'Broncos',
     awayTeam: 'Titans',
-    homeSpread: '-7',
-    awaySpread: '+7',
+    homeSpread: '-2.5',
+    awaySpread: '+2.5',
     homeScore: null,
     awayScore: null
   },
